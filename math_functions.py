d7279c3e72aa8492e9dc0ff6f4c7ea2e2862bf38
amandeep75 <amandeeptiwary01@gmail.com>
2021-09-05 01:38:02 -0700

    MA and EMA Functions

---
+++ math_functions.py
@@ -0,0 +1,32 @@
+def SMA(df, period):
+    '''
+    Simple Moving Average calculated on closing_price.
+    (SMA), is calculated by taking the arithmetic mean of a given set of values over a specified period of time.
+
+    Parameters
+    ----------
+    df : DataFrame
+    period : in milliseconds
+
+    Returns
+    -------
+    resultDf dataframe with moving average column
+    '''
+    return resultDf
+
+def EMA(df, period):
+    '''
+    Exponential Moving Average calculated over closing_price
+    The exponential moving average is a type of moving average that gives more weight to recent
+    prices in an attempt to make it more responsive to new information.
+    Parameters
+    ----------
+    df : DataFrame
+    period : in milliseconds
+
+    Returns
+    -------
+    resultDf dataframe with moving average column
+    '''
+
+    return resultDf
